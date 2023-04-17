329bc1b2ccedf765b86221e59af43f1abe428ef9
DimonTolk <dimontolk.1996@gmail.com>
2023-04-17 21:18:19 +0300

    Implemented task

---
+++ AsyncAwait.Tasks/AsyncAwait.Task1.CancellationTokens/Calculator.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncAwait.Task1.CancellationTokens;
+
+internal static class Calculator
+{
+    public static Task<long> Calculate(int n, CancellationToken token)
+    {
+        return Task.Run(() =>
+        {
+            long sum = 0;
+
+            for (var i = 1; i <= n; i++)
+            {
+                if (token.IsCancellationRequested)
+                    return sum;
+
+                sum += i;
+                Thread.Sleep(10);
+            }
+
+            return sum;
+        },
+        token);
+    }
+}
---
+++ AsyncAwait.Tasks/AsyncAwait.Task2.CodeReviewChallenge/Services/PrivacyDataService.cs
@@ -0,0 +1,6 @@
+﻿namespace AsyncAwait.Task2.CodeReviewChallenge.Services;
+
+public class PrivacyDataService : IPrivacyDataService
+{
+    public string GetPrivacyData() => "This Policy describes how async/await processes your personal data, but it may not address all possible data processing scenarios.";
+}
